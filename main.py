d66eb801b7ce591fba77dbd2eb2a4920e749036d
thomasmenier <thomasmenier.pro@gmail.com>
2023-01-27 17:42:03 +0100

    bidouille et avancement de svg_dico et création des contreparties Json

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,12 +9,12 @@ SEPARATOR = ','  # Séparateur dans le fichier de sauvegarde
 
 # Supprimer les doublons ?!
 def liste_liens(source):
+    links = []
     html = requests.get(WIKI_URL + source).text
     body = BeautifulSoup(html, 'html.parser').find('div', id='mw-content-text')
-    links = []
     for anchor in body.find_all('a'):
         link = anchor.get('href')
-        if link.startswith('/wiki/') and ':' not in link[len('/wiki/'):]:  # Si le nom de la page ne contient pas un ':'
+        if link.startswith('/wiki/') and ':' not in link[len('/wiki/'):]:
             links.append(link[len('/wiki/'):])
         elif link.startswith(WIKI_URL) and ':' not in link[len(WIKI_URL):]:
             links.append(link[len(WIKI_URL):])
@@ -26,25 +28,32 @@ def svg_dico(dico, file):
         for link in dico[page]:
             s += SEPARATOR + link
         s += "\n"
-    f = open(file, "w")
-    f.write(s)
-    f.close()
+    with open(file, "w") as f:
+        f.write(s)
+
+
+def svg_dico_json(dico, file):
+    with open(file, "w") as f:
+        json.dump(dico, f)
 
 
 def chg_dico(file):
     dico = {}
-    f = open(file)
-    for line in f.readlines():
-        line.split(SEPARATOR)
-        dico[line[0]] = line[1:]
-    f.close()
+    with open(file) as f:
+        for line in f.readlines():
+            line.split(SEPARATOR)
+            dico[line[0]] = line[1:]
     return dico
+
+
+def chg_dico_json(file):
+    with open(file) as f:
+        return json.load(f)
 
 
 def svg_wiki(dico, file):
     return 0
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    svg_dico({"Petyr_Baelish": liste_liens("Petyr_Baelish")}, "sauv.txt")
+    svg_dico_json({"Petyr_Baelish": liste_liens("Petyr_Baelish")}, "sauv_json.txt")
